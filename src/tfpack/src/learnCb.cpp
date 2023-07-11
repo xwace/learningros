f04e99c11241cd3f3592bd6f18fe00ef25dec341
xwace <37037519+xwace@users.noreply.github.com>
2023-07-11 23:05:33 +0800

    Update learnCb.cpp

--- src/tfpack/src/learnCb.cpp
+++ src/tfpack/src/learnCb.cpp
@@ -17,3 +17,29 @@ struct Client {
         cout << "val: " << *val << endl;
     }
 };
+
+//验证subscribe即为开启一个线程的测试代码
+class DynamicReconfigureClient {
+public:
+    bool flag = false;
+
+    DynamicReconfigureClient() {
+
+        auto id = new std::thread(&DynamicReconfigureClient::func, this, nullptr);
+        while (flag == false) {
+            cout << "wait" << endl;
+            waitKey(10000);
+        }
+
+        cout << "finally jump out of the loop!!!!" << endl;
+    }
+
+    void func(int *val) {
+        int n{0};
+        while (n++ < 3) {
+            cout << "new thread: " << n << endl;
+        }
+
+        flag = true;
+    }
+};
